7c9c7a2f3424923cad1e9c3e229c02f287f51278
dhinta <relaxed.dhinta@gmail.com>
2020-02-08 17:03:55 +0530

    feat(shared-module): shared module & error component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { RegistrationComponent } from './components/pages/registration/registration.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PublicBaseComponent } from './components/layout/public-base/public-base.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: PublicBaseComponent,
+  children: [{
+    path: '',
+    component: HomeComponent
+  }, {
+    path: 'registration',
+    component: RegistrationComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicBaseComponent } from './components/layout/public-base/public-base.component';
+import { PrivateBaseComponent } from './components/layout/private-base/private-base.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { RegistrationComponent } from './components/pages/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicBaseComponent,
+    PrivateBaseComponent,
+    HomeComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
